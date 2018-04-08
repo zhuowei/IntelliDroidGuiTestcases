323b4dac0e6da62b5939386f4f133ac24b4fe09a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2018-04-08 14:14:36 -0400

    Avoid Gator false positives by using local variables

--- ListenerTypesApp/app/src/main/java/ca/utoronto/intellidroid/guieval/listenertypesapp/MainActivity.java
+++ ListenerTypesApp/app/src/main/java/ca/utoronto/intellidroid/guieval/listenertypesapp/MainActivity.java
@@ -8,21 +8,19 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
     private static final String TAG = "ListenerTypesApp";
-    private Button mButtonWithoutIdCheck;
-    private Button mButtonWithIdCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButtonWithoutIdCheck = findViewById(R.id.button_without_id_check);
-        mButtonWithoutIdCheck.setOnClickListener(new View.OnClickListener() {
+        Button buttonWithoutIdCheck = findViewById(R.id.button_without_id_check);
+        buttonWithoutIdCheck.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 logInfo("Button without ID clicked");
             }
         });
-        mButtonWithIdCheck = findViewById(R.id.button_with_id_check);
-        mButtonWithIdCheck.setOnClickListener(new View.OnClickListener() {
+        Button buttonWithIdCheck = findViewById(R.id.button_with_id_check);
+        buttonWithIdCheck.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (v.getId() == R.id.button_with_id_check) {
                     logInfo("Button with ID check clicked");
